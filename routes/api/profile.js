fbfe01d6aa254d0d0dd188961c13ae891fe8f152
Rodrigo Fernandes <rodrigorcfernandes@gmail.com>
2020-07-22 23:03:02 -0300

    Added dashboard and profile management.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,7 +7,7 @@ const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
-const { findOne } = require("../../models/Profile");
+const Post = require("../../models/Post");
 
 router.get("/me", auth, async (req, res) => {
   try {
@@ -126,6 +126,7 @@ router.get("/user/:user_id", async (req, res) => {
 
 router.delete("/", auth, async (req, res) => {
   try {
+    await Post.deleteMany({ user: req.user.id });
     await Profile.findOneAndRemove({ user: req.user.id });
     await User.findOneAndRemove({ _id: req.user.id });
     res.json({ msg: "User deleted" });
